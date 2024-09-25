5d4e445e031b612744fd95572979c9a2edc71d40
Arthur Novaes <arthurnovaes55@gmail.com>
2024-09-25 14:42:33 -0300

    refactor: separa model course e category

---
+++ graph/model/course.go
@@ -0,0 +1,9 @@
+package model
+
+
+type Course struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description *string   `json:"description,omitempty"`
+	Category    *Category `json:"category"`
+}
