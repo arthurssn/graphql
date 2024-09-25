38a864c37dcb38fadbf4bcc24a0c29f5ef24bcdc
Arthur Novaes <arthurnovaes55@gmail.com>
2024-09-25 15:14:33 -0300

    feat: implementa find by course id

--- graph/model/course.go
+++ graph/model/course.go
@@ -5,5 +5,4 @@ type Course struct {
 	ID          string    `json:"id"`
 	Title       string    `json:"title"`
 	Description *string   `json:"description,omitempty"`
-	Category    *Category `json:"category"`
 }
